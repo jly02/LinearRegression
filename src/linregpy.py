25e3ec35d93e1151fca708ab9068ae013099ccd5
jly02 <lyjonathan02@gmail.com>
2022-07-02 12:58:25 -0700

    Initial commit

---
+++ src/linregpy.py
@@ -0,0 +1,389 @@
+"""
+The QuadGradDescent library aims to provide a simple implementation
+of quadratic minimization using gradient descent and a full 
+implementation of linear regression.
+"""
+
+# necessary imports, make sure you have matplotlib and numpy installed
+from math import ceil, sqrt
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from typing import List, Tuple
+from dataclasses import dataclass
+
+
+# global variables
+_plots = [] # stores the plots that have been requested to be drawn
+
+
+class Quadratic:
+    """ A class representing a quadratic function of the form
+    f(x) = ax^2 + bx + c
+        
+    Attributes
+    ----------
+    a : float
+            the second power term's coefficient
+    b : float
+        the first power term's coefficient
+    c : float
+        the constant
+        
+    Methods
+    -------
+    eval(x)
+        returns the value of this quadratic at a given value
+    toString()
+        returns a string representation of this quadratic
+    """
+
+    def __init__(self, a: float, b: float, c: float) -> None:
+        self._a = a
+        self._b = b
+        self._c = c
+    
+    
+    def eval(self, x: float) -> float:
+        """ Evaluates this Quadratic at a given value
+        
+        Parameters
+        ----------
+        x : float 
+            the x-value this function is to be evaluated at
+        
+        Returns
+        -------
+        float
+            the value of this function at a given input
+        """
+        return (self._a * (x**2)) + (self._b * x) + self._c
+    
+    
+    def toString(self) -> str:
+        """ Returns a string representation of this Quadratic.
+        """
+        firstTerm = ""
+        if(self._a != 0):
+            firstTerm = "x^2" if(self._a == 1) else f"{self._a}x^2"
+            
+        secondTerm = ""
+        if(self._b != 0):
+            secondTerm = "x" if(self._b == 1) else f"{self._b}x"
+            
+        constant = ""
+        if(self._c != 0):
+            constant = str(self._c)
+        
+        # ??? don't know if theres a better way of doing this
+        # this is indeed python so there probably is
+        result = ""
+        result += firstTerm  + " + " if(firstTerm != "") else ""
+        result += secondTerm + " + " if(secondTerm != "") else ""
+        result += constant if(constant != "") else ""
+        return result
+        
+        
+class LinearRegression:
+    """ A class representing a linear regression over a set of given
+    2-dimensional data.
+    
+    Attributes
+    ----------
+    beta : float
+        the slope coefficient of the regression line
+    inty : float
+        the y-intercept of the regression line
+    domain : (float, float)
+        the min/max x-values in the linear regression
+    range : (float, float)
+        the min/max y-values in the linear regression
+    """
+    
+    def __init__(self, x: List[float], y: List[float]) -> None:
+        self._xAxis = x
+        self._yAxis = y
+        self.beta   = self._calculateBeta()
+        self.inty   = self._calculateIntercept()
+        self.domain = self._calcDomain()
+        self.range  = self._calcRange()
+        self.rSquare = self._calcRSquare()
+        self.rSquareAdj = self._calcRSquareAdj()
+     
+       
+    def _calculateBeta(self) -> float:
+        """ Calculates the slope coefficient.
+
+        Returns
+        -------
+        float
+            the slope coefficient 
+        """
+        xBar = sum(self._xAxis) / len(self._xAxis)
+        xErr = []
+        for x in self._xAxis:
+            xErr.append(x - xBar)
+            
+        yBar = sum(self._yAxis) / len(self._yAxis)
+        yErr = []
+        for y in self._yAxis:
+            yErr.append(y - yBar)
+            
+        # just need the result of the dot product for covariance of x and y
+        covXY = np.dot(xErr, yErr)
+        # variance of x
+        varX = sum(x**2 for x in xErr)
+        
+        return covXY / varX
+    
+    
+    def _calculateIntercept(self) -> float:
+        """ Calculates the y-intercept.
+
+        Returns
+        -------
+        float
+            the y-intercept
+        """
+        xBar = sum(self._xAxis) / len(self._xAxis)
+        yBar = sum(self._yAxis) / len(self._yAxis)
+        
+        return yBar - (self.beta * xBar)
+    
+    
+    def _calcDomain(self) -> Tuple[float, float]:
+        """ Returns a tuple representing the minimum and maximum
+        x-values in the set.
+
+        Returns:
+            Tuple[float, float]: a tuple of the min/max x-values in the set
+        """
+        minX = sys.float_info.max
+        maxX = sys.float_info.min
+        for x in self._xAxis:
+            if(x < minX):
+                minX = x
+            
+            if(x > maxX):
+                maxX = x
+                
+        return (minX, maxX)
+    
+    
+    def _calcRange(self) -> Tuple[float, float]:
+        """ Returns a tuple representing the minimum and maximum
+        y-values in the set.
+
+        Returns:
+            Tuple[float, float]: a tuple of the min/max y-values in the set
+        """
+        minY = sys.float_info.max
+        maxY = sys.float_info.min
+        for y in self._yAxis:
+            if(y < minY):
+                minY = y
+            
+            if(y > maxY):
+                maxY = y
+                
+        return (minY, maxY)
+    
+    
+    def _calcRSquare(self) -> float:
+        """ Calculates the R-squared (coefficient of determination)
+        of this model over the given dataset.
+
+        Returns:
+            float: the r-squared value
+        """
+        yPred = [self.predict(x) for x in self._xAxis]
+        yBar = sum(self._yAxis) / len(self._yAxis)
+        # sum squared of prediction error and total sum
+        ssReg = []
+        ssTotal = []
+        for y, yHat in zip(self._yAxis, yPred):
+            ssReg.append((y - yHat)**2)
+            ssTotal.append((y - yBar)**2)
+            
+        return 1 - (sum(ssReg) / sum(ssTotal))
+    
+    
+    def _calcRSquareAdj(self) -> float:
+        """ Calculates the adjusted R-squared of this model.
+
+        Returns:
+            float: the adjusted r-squared value
+        """
+        # The assumption of a single independent variable is made.
+        sampleSize = len(self._xAxis)
+        return 1 - (((1 - self.rSquare) * (sampleSize - 1)) / (sampleSize - 2))
+    
+    
+    def predict(self, x: float) -> float:
+        """ Returns a prediction given by the regression line based on a given
+        value.
+
+        Arguments
+        ---------
+        x : float
+            the value of the independent variable to make a prediction at
+
+        Returns
+        -------
+        float: 
+            the value of the regression line evaluated at the given point
+        """
+        return (self.beta * x) + self.inty
+    
+    
+    def __repr__(self) -> str:
+        return "y = {:0.2f}x + {:0.2f}".format(self.beta, self.inty)
+    
+    
+@dataclass(frozen=True, order=True)
+class _LinRegPlot:
+    # Wrapper class for storing linear regressions before they are plotted
+    linReg: LinearRegression
+    xLabel: str
+    yLabel: str
+    scatter: bool
+    
+
+def plotLinReg(linReg: LinearRegression, xLabel="x-values", yLabel="y-values", scatter=True) -> None:
+    """ Enqueues a linear regression to be plotted.
+
+    Arguments
+    ---------
+    linReg : LinearRegression
+        the linear regression to be plotted
+    xLabel : str, optional
+        the x-axis label. Defaults to "x-values".
+    yLabel : str, optional
+        the y-axis label. Defaults to "y-values".
+    """
+    global _plots
+    _plots.append(_LinRegPlot(linReg, xLabel, yLabel, scatter))
+
+
+def drawPlots() -> None:
+    """ Shows all of the queued plots.
+    """
+    global _plots
+    # Calculating optimal number of rows and columns given N (numPlots) cells
+    numPlots = len(_plots)
+    cols = int(sqrt(numPlots))
+    rows = int(ceil(numPlots / cols))
+    fig, axes = plt.subplots(cols, rows)
+    currPlot = 0
+    
+    # a single plot can just be plotted on its own without any special considerations
+    if(numPlots == 1):
+        linReg = _plots[currPlot].linReg
+        var = np.linspace(linReg.domain[0], linReg.domain[1], 10) # since this is linear, you can choose any number of points
+        func = linReg.beta * var + linReg.inty
+        plt.plot(var, func)
+        
+        # plotting the scatter is optional
+        if(_plots[currPlot].scatter == True):
+            plt.scatter(linReg._xAxis, linReg._yAxis)
+            
+        plt.xlabel(_plots[0].xLabel)
+        plt.ylabel(_plots[0].yLabel)
+        plt.title(f"Plot of {linReg}")
+        
+    # two or three plots are simply calculated as a 1-D array
+    elif(numPlots < 4):
+        for row in range(rows):
+            linReg = _plots[currPlot].linReg
+            var = np.linspace(linReg.domain[0], linReg.domain[1], 10) # since this is linear, you can choose any number of points
+            func = linReg.beta * var + linReg.inty
+            axes[row].plot(var, func)
+            
+            # plotting the scatter is optional
+            if(_plots[currPlot].scatter == True):
+                axes[row].scatter(linReg._xAxis, linReg._yAxis)
+                
+            axes[row].set_xlabel(_plots[0].xLabel)
+            axes[row].set_ylabel(_plots[0].yLabel)
+            axes[row].set_title(f"Plot of {linReg}")
+            currPlot += 1
+            
+    # any more than three plots become a 2-D array
+    else:
+        for row in range(rows):
+            for col in range(cols):
+                # If the plot doesn't exist we can just delete excess axes
+                try:
+                    linReg = _plots[currPlot].linReg
+                except:
+                    fig.delaxes(axes[col][row])
+                    continue
+                    
+                var = np.linspace(linReg.domain[0], linReg.domain[1], 10) # since this is linear, you can choose any number of points
+                func = linReg.beta * var + linReg.inty
+                axes[col][row].plot(var, func)
+                    
+                # plotting the scatter is optional
+                if(_plots[currPlot].scatter == True):
+                    axes[col][row].scatter(linReg._xAxis, linReg._yAxis)
+                    
+                axes[col][row].set_xlabel(_plots[0].xLabel)
+                axes[col][row].set_ylabel(_plots[0].yLabel)
+                axes[col][row].set_title(f"Plot of {linReg}")
+                currPlot += 1
+                
+    fig.suptitle("Note: Scales are not the same.")
+    plt.tight_layout()
+    plt.show()
+
+
+def gradDesc(quad: Quadratic, epsilon=.001, alpha=.01) -> float:
+    """ Returns the x-value of the local minimum of a given
+    function using gradient descent.
+    
+    Parameters
+    ----------    
+    quad : Quadratic
+        the function being minimized
+    epsilon : float, optional 
+        the maximum permitted error when deciding when to stop iterating (default .001)
+    alpha : float, optional
+        the bound size used to approximate slope; I.E. the step size. (default .01)
+        
+    Returns
+    -------
+    float
+        the approximate x-value at which the minimum of the given quadratic occurs
+    """
+    # The default values for epsilon and alpha seem to work pretty well, but other
+    # combinations of values may also work.
+    xVal = quad._b # pick arbitrary value to start at
+    while(True):
+        # Check slope to left and right of xVal.
+        # Change xVal by a scaled amount depending on slope steepness.
+        currMin = quad.eval(xVal)
+        left  = abs(currMin - quad.eval(xVal - alpha))
+        right = abs(currMin - quad.eval(xVal + alpha))
+        if(left < right):
+            currSlope = left
+            xVal -= currSlope * alpha
+        else:
+            currSlope = right
+            xVal += currSlope * alpha
+        
+        # emulating do-while functionality
+        if(currSlope < epsilon):
+            break
+        
+    return xVal
+    
+    
+# Mostly testing code here.
+def __main():
+    poly = Quadratic(50., 10., 1.)
+    print(f"The local min of {poly.toString()} is at approximately x = {gradDesc(poly)}")
+    
+
+# Main is not run if this file is imported as a library.
+if __name__ == "__main__":
+    __main()
