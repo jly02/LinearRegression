47f3955b3816da58007c77664928506547d1879d
jly02 <lyjonathan02@gmail.com>
2022-07-02 22:55:14 -0700

    Working on setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='linregpy',
+    version='1.0.0',
+    description='this package allows you to painlessly plot linear regressions',
+    author='jly02',
+    url='https://github.com/jly02/linregpy',
+    install_requires=['numpy', 'matplotlib']
+)
--- test/linregtest.py
+++ test/linregtest.py
@@ -1,8 +1,11 @@
 import unittest
+import linregpy as lrp
+
 
 class TestLinearRegression(unittest.TestCase):
     # Test cases go here
     pass
+
 
 if __name__ == "__main__":
     unittest.main()
